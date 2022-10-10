77f4f59cd661082503f72ff786077123f38bfb67
shefaa-adel <shefaaadel19@gmail.com>
2022-10-10 17:46:33 +0200

    commit

--- src/App/App.js
+++ src/App/App.js
@@ -4,19 +4,15 @@ import Navbar from "../component/navbar";
 import { theme } from "../theme/index";
 import { ThemeProvider } from "@mui/system";
 import Footer from "../component/footer";
-<<<<<<< HEAD
 import NavbarTop from "../component/navbar-top";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
-=======
 import SignUpPage from '../pages/SignUpPage'
->>>>>>> 21f7c34fb761261d1a1905dc801b53b7c70a97d2
 
 function App() {
   const themeResponsive = useTheme();
   const matches = useMediaQuery(themeResponsive.breakpoints.up("976"));
   return (
-<<<<<<< HEAD
     <ThemeProvider theme={theme}>
       {matches ? (
         <>
@@ -30,29 +26,13 @@ function App() {
         </>
       )}
 
-      <Header />
-     <p>,jbhb</p> 
-     <p>,jbhb</p> 
-     <p>,jbhb</p> 
-     <p>,jbhb</p> 
-     <p>,jbhb</p> 
-     <p>,jbhb</p> 
-     <p>,jbhb</p> 
+      {/* <Header /> */}
+     <SignUpPage/>
 
 
      
       <Footer />
     </ThemeProvider>
-=======
-    <Box>
-      <ThemeProvider theme={theme}>
-        <Navbar />
-        {/* <Header /> */}
-        <SignUpPage/>
-        {/* <Footer/> */}
-      </ThemeProvider>
-    </Box>
->>>>>>> 21f7c34fb761261d1a1905dc801b53b7c70a97d2
   );
 }
 
